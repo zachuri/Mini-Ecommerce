d2546669636fa2ccf4538bd04c5bf7433e2addae
zachuri <punsalang.zachary@gmail.com>
2023-04-16 22:02:20 -0700

    createArticle: added update state change for input

--- src/pages/createArticle/index.tsx
+++ src/pages/createArticle/index.tsx
@@ -1,22 +1,12 @@
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
-
 import {
   createServerSupabaseClient,
   User,
 } from "@supabase/auth-helpers-nextjs";
 import { type GetServerSidePropsContext } from "next";
-
-const createArticle: NextPage = () => {
-  const supabaseClient = useSupabaseClient();
-  const user = useUser();
-  const router = useRouter();
-
-  return <div>createArticle</div>;
-};
-
-export default createArticle;
+import { type ChangeEvent, useState } from "react";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   // Create authenticated Supabase Client
@@ -41,3 +31,51 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     },
   };
 };
+
+const CreateArticle: NextPage = () => {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+  const router = useRouter();
+  const initialState = {
+    title: "",
+    content: "",
+  };
+
+  const [articleData, setArticleData] = useState(initialState);
+
+  // Handle change any to make it any input type
+  const handleChange = (e: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    setArticleData({ ...articleData, [e.target.name]: e.target.value });
+    console.log(articleData);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-4xl">Title</h1>
+          <input
+            name="title"
+            className="rounded-xl border p-5"
+            placeholder="Title"
+            onChange={handleChange}
+          />
+          <h1 className="text-4xl">Description</h1>
+          <textarea
+            name="description"
+            className="rounded-xl border p-5"
+            placeholder="Description"
+            onChange={handleChange}
+          />
+          <p>Posting as {user?.email}</p>
+          <button className="rounded-xl border bg-blue-600 p-2 text-white">
+            Create Post
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreateArticle;
